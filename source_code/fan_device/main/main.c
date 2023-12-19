593bded859a89726786596f02551fc96faa62172
PJalv <pjalvbusiness@gmail.com>
2023-12-18 20:45:34 -0800

    Testing vTaskDelay in fan_device

--- source_code/fan_device/main/main.c
+++ source_code/fan_device/main/main.c
@@ -2,13 +2,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-
 void app_main(void)
 {
     int i = 0;
-    while (1) {
+    while (1)
+    {
         printf("[%d] Hello world!\n", i);
         i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+  
 }
