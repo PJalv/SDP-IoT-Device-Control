a437f88f6d56538e445cedff3801d06489fe2231
PJalv <pjalvbusiness@gmail.com>
2024-01-20 15:23:01 -0800

    update dashboard backend

--- source_code/dashboard_gui/gui.py
+++ source_code/dashboard_gui/gui.py
@@ -19,10 +19,22 @@ BROKER_PORT = int(os.getenv("BROKER_PORT"))
 
 
 # Initialize Eel
-eel.init(f'{os.path.dirname(__file__)}/web')
+eel.init(os.path.join(os.path.dirname(__file__), 'web'))
 
 # Initialize MQTT client
 mqtt_client = mqtt.Client()
+
+@eel.expose
+def read_storage():
+    try:
+        with open((os.path.join(os.path.dirname(__file__), 'storage.json')), 'r') as file:
+            # Load JSON data from the file
+            data = json.load(file)
+            print("Sent JSON to EEL")
+            return json.dumps(data)  # Return JSON as a string
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 @eel.expose
 def get_random_data():
@@ -36,10 +48,11 @@ def get_external_data():
 @eel.expose
 def publish_to_mqtt(topic, message):
     mqtt_client.publish(topic, message)
+    print(f"SENT {message} to {topic}")
 
 @eel.expose
 def getLEDPower():
-    with open(f'{os.path.dirname(__file__)}/storage.json', 'r') as file:
+    with open('storage.json', 'r') as file:
     # Load the JSON data
         data = json.load(file)
     print(data["led-device"]["power"])
@@ -49,12 +62,11 @@ def getLEDPower():
 # Start MQTT client
 mqtt_client.connect(BROKER_ADDRESS, BROKER_PORT)
 mqtt_client.loop_start()
-print("Started Loop!")
 
 
 
 # Start Eel with the HTML file
-eel.start('index.html',size=(800,400),mode='edge', block=False)
+eel.start('index.html', mode='edge', block=False)
 
 while True:
     eel.sleep(1.0)
