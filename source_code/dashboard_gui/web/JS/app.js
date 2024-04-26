642dcfa2760aa5656e53dd13126e4239448a31f8
PJalv <pjalvbusiness@gmail.com>
2024-04-25 21:27:56 -0700

    Update Dashboard GUI

--- source_code/dashboard_gui/web/JS/app.js
+++ source_code/dashboard_gui/web/JS/app.js
@@ -1,105 +1,116 @@
-document.body.style.userSelect = 'none';
+document.body.style.userSelect = "none";
 
 async function mqtt_connect() {
-    statusElement = document.querySelector("#hub-status").innerText = "Connecting...";
-    await eel.mqtt_connect()();
-    await getIP();
-    await getBrokerStatus();
+  statusElement = document.querySelector("#hub-status").innerText =
+    "Connecting...";
+  await eel.mqtt_connect()();
+  await getIP();
+  await getBrokerStatus();
 }
 
 async function getIP() {
-    const ipaddr = await eel.getIP()();
-    console.log(ipaddr);
-    ipAddressElement = document.querySelector("#hub-ip").innerText = ipaddr;
-
+  const ipaddr = await eel.getIP()();
+  console.log(ipaddr);
+  ipAddressElement = document.querySelector("#hub-ip").innerText = ipaddr;
 }
 async function getBrokerStatus() {
-    const status = await eel.getBrokerStatus()();
-    if (status) {
-        statusElement = document.querySelector("#hub-status").innerText = "Connected";
-        statusElement = document.querySelector("#hub-status").style.color = "green";
-    } else {
-        statusElement = document.querySelector("#hub-status").innerText = "Offline";
-        statusElement = document.querySelector("#hub-status").style.color = "red";
-
-    }
+  const status = await eel.getBrokerStatus()();
+  if (status) {
+    statusElement = document.querySelector("#hub-status").innerText =
+      "Connected";
+    statusElement = document.querySelector("#hub-status").style.color = "green";
+  } else {
+    statusElement = document.querySelector("#hub-status").innerText = "Offline";
+    statusElement = document.querySelector("#hub-status").style.color = "red";
+  }
 }
 
 async function updateData() {
-    // console.log("Enter update");
-    try {
-        const data = await eel.read_storage()();
-        return JSON.parse(data);
-    } catch (error) {
-        console.error('Error updating data:', error);
-        return null;  // Return null to indicate an error
-    }
+  // console.log("Enter update");
+  try {
+    const data = await eel.read_storage()();
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error updating data:", error);
+    return null; // Return null to indicate an error
+  }
 }
 
 async function publish(topic, message = null) {
-    try {
-        const data = await updateData();
-        let powValue;
-        switch (topic) {
-            case "fan/control":
-                powValue = data['fan-device']['power'];
-                powValue = (powValue === 1) ? 0 : 1;
-                console.log(powValue);
-                await eel.publish_to_mqtt(topic, `INT:${powValue}`)();
-                break;
-            case "led/control/power":
-                powValue = data['led-device']['power'];
-                powValue = (powValue === 1) ? 0 : 1;
-                console.log(powValue);
-                await eel.publish_to_mqtt(topic, `INT:${powValue}`)();
-                break;
-            // case "led/control/color":
-            //     const colorObject = {
-            //         red: Math.floor(Math.random() * 256),
-            //         green: Math.floor(Math.random() * 256),
-            //         blue: Math.floor(Math.random() * 256),
-            //     };
-            //     await eel.publish_to_mqtt(topic, `JSON:${JSON.stringify(colorObject)}`)();
-            //     break;
-            default:
-                await eel.publish_to_mqtt(topic, `INT:${message}`)
-                break;
-        }
-    } catch (error) {
-        console.error('Error publishing data:', error);
-    }
-}
-document.addEventListener('DOMContentLoaded', async function () {
+  try {
     const data = await updateData();
-    document.querySelector('#fan-device').classList = data["fan-device"]["status"]["isOnline"]==0?"card device-card shadow gray-out":"card device-card shadow";
-    const [brokerName] = await eel.getBrokerParams()();
-    console.log(brokerName);
-    const status = await eel.getBrokerStatus()();
-    await getIP();
-    console.log(status);
-    if (!status) {
-        await mqtt_connect();
-        await getBrokerStatus();
+    let powValue;
+    switch (topic) {
+      case "fan/control":
+        powValue = data["fan-device"]["power"];
+        powValue = powValue === 1 ? 0 : 1;
+        console.log(powValue);
+        await eel.publish_to_mqtt(topic, `INT:${powValue}`)();
+        break;
+      case "led/control/power":
+        powValue = data["led-device"]["power"];
+        powValue = powValue === 1 ? 0 : 1;
+        console.log(powValue);
+        await eel.publish_to_mqtt(topic, `INT:${powValue}`)();
+        break;
+      // case "led/control/color":
+      //     const colorObject = {
+      //         red: Math.floor(Math.random() * 256),
+      //         green: Math.floor(Math.random() * 256),
+      //         blue: Math.floor(Math.random() * 256),
+      //     };
+      //     await eel.publish_to_mqtt(topic, `JSON:${JSON.stringify(colorObject)}`)();
+      //     break;
+      default:
+        await eel.publish_to_mqtt(topic, `INT:${message}`);
+        break;
     }
-    else {
-        await getBrokerStatus();
-    }
-    document.querySelector("#brokerName").innerText = brokerName;
+  } catch (error) {
+    console.error("Error publishing data:", error);
+  }
+}
+document.addEventListener("DOMContentLoaded", async function () {
+  const data = await updateData();
+  document.querySelector("#fan-device").classList =
+    data["fan-device"]["status"]["isOnline"] == 0
+      ? "card device-card shadow gray-out"
+      : "card device-card shadow";
+  const [brokerName] = await eel.getBrokerParams()();
+  console.log(brokerName);
+  const status = await eel.getBrokerStatus()();
+  await getIP();
+  console.log(status);
+  if (!status) {
+    await mqtt_connect();
+    await getBrokerStatus();
+  } else {
+    await getBrokerStatus();
+  }
+  document.querySelector("#brokerName").innerText = brokerName;
 });
 setInterval(async () => {
-    // console.log("enter loop");
-    const data = await updateData();
+  // console.log("enter loop");
+  const data = await updateData();
 
-    console.log(data);
-    if (data !== null) {
-        // console.log(`Updated data:`, JSON.stringify(data));
-        // localStorage.setItem('pageData', JSON.stringify(data));
-        document.querySelector("#currentColorSquare").style.backgroundColor = `rgb(${data["led-device"]["color"]["red"]}, ${data["led-device"]["color"]["green"]}, ${data["led-device"]["color"]["blue"]})`;
-        document.querySelector('#fan-power').innerText = data['fan-device']['power'] == 0 ? "OFF" : "ON";
-        document.querySelector('#fan-rpm').innerText = data["fan-device"]["rpm"];
-        document.querySelector('#fan-device').classList = data["fan-device"]["status"]["isOnline"]==0?"card device-card shadow gray-out":"card device-card shadow";
-        document.querySelector('#led-device').classList = data["led-device"]["status"]["isOnline"]==0?"card device-card shadow gray-out":"card device-card shadow";
-
-
-    }
+  console.log(data);
+  if (data !== null) {
+    // console.log(`Updated data:`, JSON.stringify(data));
+    // localStorage.setItem('pageData', JSON.stringify(data));
+    document.querySelector("#led-power").innerText =
+      data["led-device"]["power"] == 0 ? "OFF" : "ON";
+    document.querySelector(
+      "#currentColorSquare"
+    ).style.backgroundColor = `rgb(${data["led-device"]["color"]["red"]}, ${data["led-device"]["color"]["green"]}, ${data["led-device"]["color"]["blue"]})`;
+    document.querySelector("#fan-power").innerText =
+      data["fan-device"]["power"] == 0 ? "OFF" : "ON";
+    document.querySelector("#fan-rpm").innerText = data["fan-device"]["rpm"];
+    document.querySelector("#fan-device").classList =
+      data["fan-device"]["status"]["isOnline"] == 0
+        ? "card device-card shadow gray-out"
+        : "card device-card shadow";
+    document.querySelector("#led-device").classList =
+      data["led-device"]["status"]["isOnline"] == 0
+        ? "card device-card shadow gray-out"
+        : "card device-card shadow";
+  }
 }, 500);
