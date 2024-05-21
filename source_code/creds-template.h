9fd2c79cb02aafa595e0bb23e5195267261ae35b
Jorge Luis Suarez <134026493+PJalv@users.noreply.github.com>
2024-05-21 14:53:11 -0700

    Update creds-template.h

--- source_code/creds-template.h
+++ source_code/creds-template.h
@@ -3,6 +3,7 @@
 #define CREDENTIALS_TEMPLATE_H
 
 // Replace these placeholders with your actual SSID and password
+// Once ready, rename to credentials.h
 #define WIFI_SSID "REPLACE WITH ACCESS POINT NAME"
 #define WIFI_PASSWORD "REPLACE WITH PASSWORD TO ACCESS POINT"
 #define BROKER_URI "mqtt://BROKER IP ADDRESS:PORT"
